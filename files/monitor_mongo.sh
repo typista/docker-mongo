5552d9eef311d6288f99977888a1a17d20551ad1
geeek <webmaster@geeek.red>
2014-12-31 09:52:20 +0900

    mongo監視シェル

---
+++ files/monitor_mongo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#$MONITOR_PS_LIST
+NOW=`date '+%Y-%m-%d %H:%M:%S'`
+LOG=/root/export/shell.log
+MONGO=/usr/bin/mongod
+IS_EXEC=`cat $MONITOR_PS_LIST | grep $MONGO | grep -v grep`
+MONGO_PS=`echo $IS_EXEC | awk '{print $2}'`
+LOAD_AVERAGE=`uptime | sed -e 's/.*average: //g' -e 's/,//g' | awk '{print $2}'`
+THRESHOLD=`grep 'processor' /proc/cpuinfo | wc -l`
+COMPARE=`echo "${THRESHOLD} < ${LOAD_AVERAGE}" | bc` 
+touch $LOG
+echo "=================" >> $LOG
+echo $NOW >> $LOG
+echo "load average:"$LOAD_AVERAGE >> $LOG
+echo "threshold:"$THRESHOLD >> $LOG
+echo "compare:"$COMPARE >> $LOG
+if [ ${COMPARE} = 1 -a "$MONGO_PS" != "" ]; then
+	echo "kill" >> $LOG
+	kill -9 $MONGO_PS
+	sleep 3
+	IS_EXEC=""
+fi
+if [ "$IS_EXEC" = "" ]; then
+	echo "start" >> $LOG
+	$MONGO --smallfiles &
+fi
