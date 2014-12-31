b3b72ff189bce3307d4020581f24bdd4116c0ce5
geeek <webmaster@geeek.red>
2014-12-31 09:48:10 +0900

    crond開始抜け

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 REPO=docker-mongo
-URL=https://raw.githubusercontent.com/typista/$REPO/master/files
+export URL_GIT=https://raw.githubusercontent.com/typista/$REPO/master/files
+
 LOCALTIME=/etc/localtime
 if [ ! -L $LOCALTIME ]; then
 	rm $LOCALTIME
@@ -8,7 +9,7 @@ if [ ! -L $LOCALTIME ]; then
 fi
 EXEC1ST=/root/export/exec1st.sh
 if [ ! -f $EXEC1ST ];then
-	wget $URL/exec1st.sh -O $EXEC1ST
+	wget $URL_GIT/exec1st.sh -O $EXEC1ST
 fi
 if [ ! -x $EXEC1ST ];then
 	chmod +x $EXEC1ST
--- files/exec1st.sh
+++ files/exec1st.sh
@@ -2,3 +2,8 @@
 MONGODB_CONF=/etc/mongod.conf
 sed -ri "s/# nojournal=true/nojournal=true/g" $MONGODB_CONF
 /usr/bin/mongod --smallfiles &
+CRON_TAB=/root/export/crontab.txt
+if [ ! -f $CRON_TAB ];then
+    wget $URL_GIT/crontab.txt -O $CRON_TAB
+fi
+crontab $CRON_TAB
