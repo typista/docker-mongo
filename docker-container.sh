48fd036354f8eef90d4f21f2303db4ae3d7bb0e7
core@core1 <core@core1>
2015-01-05 12:45:33 +0900

    インデント整形

--- docker-container.sh
+++ docker-container.sh
@@ -8,11 +8,16 @@ else
 	FULLPATH=$(cd `dirname $0`; pwd)/`basename $0`
 	DIR=`dirname $FULLPATH`
 	REPO=`basename $DIR`
-    REPO=`echo $REPO | sed -r "s/docker\-//g"`
-    IMAGE=$USER/$REPO
+	REPO=`echo $REPO | sed -r "s/docker\-//g"`
+	IMAGE=$USER/$REPO
 	if [ "$2" != "" ];then
-    	IMAGE=$IMAGE:$2
-    fi
+    		IMAGE=$IMAGE:$2
+	else
+		VERSION=`docker images | grep "$IMAGE " | sort | tail -1 | awk '{print $2}'`
+		if [ "$VERSION" != "" ];then
+			IMAGE=$IMAGE:$VERSION
+		fi
+	fi
 	docker run -d --privileged --restart=always --name="$__FQDN__" --hostname="$__HOSTNAME__" \
 		-p 27017:27017 \
 		-v ${PWD}/export/root/:/root/export/ \
