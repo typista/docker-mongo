9944bb914c02bfcb8604287fd097fe7d9cc5f4a7
core@core1 <core@core1>
2015-01-13 00:17:38 +0900

    loadAvarageの取得位置不具合

--- files/monitor_mongo.sh
+++ files/monitor_mongo.sh
@@ -5,7 +5,7 @@ LOG=/root/export/shell.log
 MONGO=/usr/bin/mongod
 IS_EXEC=`cat $MONITOR_PS_LIST | grep $MONGO | grep -v grep`
 MONGO_PS=`echo $IS_EXEC | awk '{print $2}'`
-LOAD_AVERAGE=`uptime | sed -e 's/.*average: //g' -e 's/,//g' | awk '{print $2}'`
+LOAD_AVERAGE=`uptime | sed -e 's/.*average: //g' -e 's/,//g' | awk '{print $1}'`
 THRESHOLD=`grep 'processor' /proc/cpuinfo | wc -l`
 COMPARE=`echo "${THRESHOLD} < ${LOAD_AVERAGE}" | bc` 
 touch $LOG
