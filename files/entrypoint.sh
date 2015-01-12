4bfa9d71bdf858c3b8202244dac4279517255fc4
core@core1 <core@core1>
2015-01-13 00:17:24 +0900

    必要シェルの不具合整理

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -14,6 +14,20 @@ fi
 if [ ! -x $EXEC1ST ];then
 	chmod +x $EXEC1ST
 fi
+START=/root/export/start.sh
+if [ ! -f $START ];then
+    wget $URL_GIT/start.sh -O $START
+fi
+if [ ! -x $START ];then
+    chmod +x $START
+fi
+MONITOR=/root/export/monitor_mongo.sh
+if [ ! -f $MONITOR ];then
+    wget $URL_GIT/monitor_mongo.sh -O $MONITOR
+fi
+if [ ! -x $MONITOR ];then
+    chmod +x $MONITOR
+fi
 $EXEC1ST > /dev/null 2>&1
 /etc/init.d/crond start
 /usr/bin/tail -f /dev/null
