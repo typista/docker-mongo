809c514d1b15ddefc87f896d18e101ebcac390f0
core@core1 <core@core1>
2015-01-13 00:06:56 +0900

    必要ファイルのwget抜け

--- files/exec1st.sh
+++ files/exec1st.sh
@@ -6,4 +6,18 @@ CRON_TAB=/root/export/crontab.txt
 if [ ! -f $CRON_TAB ];then
     wget $URL_GIT/crontab.txt -O $CRON_TAB
 fi
+START=/root/export/start.sh
+if [ ! -f $START ];then
+    wget $URL_GIT/start.sh -O $START
+fi
+if [ ! -x $START ];then
+    chmod +x $START
+fi
+MONITOR=/root/export/monitor_mongo.sh
+if [ ! -f $MONITOR ];then
+    wget $URL_GIT/monitor_mongo.sh -O $MONITOR
+fi
+if [ ! -x $MONITOR ];then
+    chmod +x $MONITOR
+fi
 crontab $CRON_TAB
